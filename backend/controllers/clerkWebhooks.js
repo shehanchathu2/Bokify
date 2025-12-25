6d5ac2887c023f0d150eca65c4904b32fa7f7bcd
shehan <168344942+shehanchathu2@users.noreply.github.com>
2025-12-26 01:05:36 +0530

    update user delete function

--- backend/controllers/clerkWebhooks.js
+++ backend/controllers/clerkWebhooks.js
@@ -20,30 +20,42 @@ const clerkWbohooks = async (req, res) => {
             _id: data.id,
             email: data.email_addresses[0].email_address,
             username: data.first_name + " " + data.last_name,
-            image:data.image_url,
+            image: data.image_url,
         }
 
         switch (type) {
             case "user.created": {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                }
                 await User.create(userData)
                 break
             }
             case "user.updated": {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                }
                 await User.findByIdAndUpdate(data.id, userData)
                 break;
-            }         
+            }
             case "user.deleted": {
                 await User.findByIdAndDelete(data.id, userData)
                 break;
             }
-                
+
             default:
                 break
         }
-        res.json({success:true,message:"Webhook Recieved"})
+        res.json({ success: true, message: "Webhook Recieved" })
     } catch (error) {
         console.log(error.message)
-        res.json({success:false,message:error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
